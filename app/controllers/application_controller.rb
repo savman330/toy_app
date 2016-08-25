f215c80de9e27c0f6850fe8f0267307befbe993f
savman330 <larkin330@gmail.com>
2016-08-25 23:49:25 +0000

    more fixes, lol me

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-end
 
-def hello
-    render "hello world!"
+
+    def hello
+        render html: "hello world!"
+    end
 end
